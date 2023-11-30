60584d97e1b5cb13775c3c10f047d11b83c49b38
Louisgeoffrey <louisgeoffrey4@gmail.com>
2023-11-29 18:55:02 -0800

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -7,7 +7,6 @@ class Rectangle:
         number_of_instances (int): The number of Rectangle instances.
         print_symbol (any): The symbol used for string representation.
     """
-
     number_of_instances = 0
     print_symbol = "#"
 
