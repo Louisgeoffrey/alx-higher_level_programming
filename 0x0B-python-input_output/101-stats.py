3d0b94c49901e0d5150c3bf7b5cf924b9717ef1d
Louisgeoffrey <louisgeoffrey4@gmail.com>
2023-12-06 19:02:56 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Reads from standard input and computes metrics.
-After every ten lines of the input of a keyboard interruption (CTRL * C).
+After every ten lines of the input of a keyboard interruption (CTRL + C).
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     size = 0
     status_codes = {}
-    valid_codes = ['200', '301', '401', '403', '404', '405', '500']
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
